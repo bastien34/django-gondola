aeaee361bc654c67b8c372ec50024b435d84b308
Bastien Roques <bastien.roques1@gmail.com>
2017-03-01 18:24:13 +0100

    Wrong namespace in tables.py

--- gondola/tables.py
+++ gondola/tables.py
@@ -7,7 +7,7 @@ from .models import GondoleRow
 class GondoleRowTable(tables.Table):
 
     def render_images(self, value):
-        t = get_template('gondola_grid/dashboard/_gondola_cell_table.html')
+        t = get_template('gondola/dashboard/_gondola_cell_table.html')
         return t.render({'images': value.all(), 'row': value.instance, })
 
     class Meta:
